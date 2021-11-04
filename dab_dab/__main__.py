2aaef138681348fadf3ecd0f42cbd57b4fd3d433
Yaser Amiri <yaser.amiri95@gmail.com>
2021-11-04 18:12:13 +0330

    Add logging and config

--- dab_dab/__main__.py
+++ dab_dab/__main__.py
@@ -1,5 +1,22 @@
 import sys
+import logging
+import json
+
 from . import server
 
-args = sys.argv[1:]
-server.run()
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    datefmt="%Y-%m-d %H:%M:%S",
+)
+
+
+def main():
+    args = sys.argv[1:]
+    logging.info("Args: %s" % args)
+    with open("/tmp/dabdab_config.json") as f:
+        config = json.load(f)
+    server.run(config.get("host", "localhost"), config.get("port", 9669))
+
+
+main()
--- dab_dab/server.py
+++ dab_dab/server.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, Any
 import traceback
 import json
@@ -6,7 +7,9 @@ from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 
 
 class Handler(BaseHTTPRequestHandler):
-    def _return_response(self, body: Dict[str, Any], status_code: int = 200):
+    def _return_response(
+        self, body: Dict[str, Any], status_code: int = 200
+    ) -> None:
         self.send_response(status_code)
         self.send_header("Content-Type", "application/json")
         self.send_header("Access-Control-Allow-Origin", "*")
@@ -16,12 +19,13 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         output = json.dumps(body)
         self.wfile.write(output.encode())
+        return
 
     def _is_interactive(self) -> bool:
         qs = parse_qs(urlparse(self.path).query).get("interactive", [])
         return bool(qs and qs[0].lower() == "true")
 
-    def do_POST(self):
+    def do_POST(self) -> None:
         print(self.path)
         try:
             if self.headers.get("Content-Type") != "application/json":
@@ -46,14 +50,19 @@ class Handler(BaseHTTPRequestHandler):
             traceback.print_exc()
             self.send_error(500, "Something bad happend! check the logs.")
 
-    def do_OPTIONS(self):
+    def do_OPTIONS(self) -> None:
         self._return_response({})
         return
 
 
-def run(host: str = "localhost", port: int = 9669):
+def run(host: str, port: int) -> None:
+    logging.info("Trying to create a HTTP server on %s:%s" % (host, port))
     server = ThreadingHTTPServer((host, port), Handler)
+    logging.info(
+        "HTTP server started successfully, address: %s:%s" % (host, port)
+    )
     try:
         server.serve_forever()
     except KeyboardInterrupt:
         server.socket.close()
+        logging.info("Bye!")
